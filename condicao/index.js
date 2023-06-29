83f8ba54705feaef45551357ebb5e44c4c5afd44
biancamandrade <biancamdandrade@gmail.com>
2023-06-29 19:44:55 -0300

    Logica de programação, AULA

---
+++ condicao/index.js
@@ -0,0 +1,56 @@
+//const pessoa = 'Bianca'
+//let idade = 26
+//idade = 16
+//if (idade > 18) {
+  //  console.log('Verdade', idade)
+//} else {
+  //  console.log('Falso', idade)
+//}
+
+// Operadores lógicos
+// Soma: +
+// Subtrair: -
+// Dividir: /
+// Multiplicar: *
+// Resto: %
+
+//let num1 = 3
+//let num2 = 6
+
+//const resultado = num1 + num2
+
+// console.log(resultado % 2 === 0) Saber quando é par
+// console.log(resultado % 5 === 0) Saber quando é impar
+// console.log(0 === '0')
+
+//if (num1  === 0) {
+//    console.log('Numero:', 0)
+//} else if (num1 % 2 === 0) {
+//    console.log('par', num1)
+// } else {
+//    console.log('impar', num1)
+//}
+
+let valor = -1;
+
+if (valor  === 0) {
+    console.log('Zero')
+} else if (valor > 0) {
+    console.log('Número é positivo')
+ }else {
+   console.log('Número é negativo')
+}
+
+
+// ou desse jeito tambem
+// if (valor  === 0) {
+//    console.log('Zero')
+// } else if (valor > 0) {
+//    console.log('Número é negativo')
+// }else {
+//   console.log('Número é positivo')
+// }
+
+
+
+
